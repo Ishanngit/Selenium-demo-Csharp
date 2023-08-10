6275be7d9c4dbaf79576ec69c7a5ee40b64cfbcb
SF-LAP-909\ishan <ishan.n@simformsolutions.com>
2023-08-10 11:55:37 +0530

    Latest changes

---
+++ Selenium demo/Array.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_demo
+{
+    [TestClass]
+    public class Array
+    {
+        String[] a = { "This is test", "Array of string", "Multiple", "Ishan" };
+        int[] b = { 1, 2, 3 };
+        [TestMethod]
+        public void arraytest()
+        {
+            String[] a1 = new String[4];
+            a1[0] = "Ishan";
+            a2[1] = "Test";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+                if (i == 0)
+            }
+        }
+    }
+}
---
+++ Selenium demo/stringtest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_demo
+{
+    [TestClass]
+    public class stringtest
+
+    {
+        [TestMethod]
+       public void test()
+        {
+            Console.WriteLine("TEsting");
+            int i = 5;
+            Console.WriteLine("This is con data"+i);
+
+            String name = "Ishaan";
+
+            Console.WriteLine($"THis is test {name}");
+
+            dynamic height = 5.9;
+            Console.WriteLine($"The height is {height}");
+
+            height = "The height";
+            Console.WriteLine($"The new data {height}");
+
+        }
+
+    }
+
+}
