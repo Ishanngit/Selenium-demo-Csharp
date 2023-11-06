a0281438096637f0157b698adc6d70d77879de10
Ishanngit <120621687+Ishanngit@users.noreply.github.com>
2023-11-06 20:08:21 +0530

    API on going

---
+++ CsharpSeleniumFramework/Tests/ApiTests.cs
@@ -0,0 +1,16 @@
+﻿public class ApiTests
+{
+
+    [Test]
+    public void SeleniumAndApiTest()
+    {
+        // Perform web testing actions with Selenium
+
+        // Call the API test method
+        var apiTests = new ApiTests();
+        apiTests.SeleniumAndApiTest();
+
+        // Continue web testing and assertions
+    }
+
+}
---
+++ CsharpSeleniumFramework/Utilities/Api.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using NUnit.Framework;
+using System.Net;
+
+public class Api
+{
+    private RestClient client;
+    private RestResponse response;
+
+    [SetUp]
+    public void SetUp()
+    {
+        client = new RestClient("https://reqres.in/api/users");
+    }
+
+    [Test]
+    public void ApiTestExample()
+    {
+        var request = new RestRequest("/endpoint", Method.Get);
+        response = client.Execute(request);
+
+        // Assert on the API response
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        // Additional assertions
+    }
+}
